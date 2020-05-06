eeebddd24332b2c7fab57395be2a8fd9a91e5880
Narquelion <a.kramer103@gmail.com>
2020-05-06 12:30:30 -0400

    Cleanup

--- public/templates/basic/resources/js/firebase-config.js
+++ public/templates/basic/resources/js/firebase-config.js
@@ -1,26 +1,24 @@
 // firebase-config.js
-
-/* This file contains the data necessary to connect to your Firebase project.
-
- * apiKey - The public API key of the project.
- * Can be found in the project's settings in the Firebase Console.
-
- * databaseURL - The URL of the project's database.
- * Can be found by navigating to the real-time database in the Firebase Console.
-
- * storageBucket - The URL of the project's storage bucket.
- * Can be found by navigating to Storage in the Firebase Console.
- */
-
+//This file contains the data necessary to connect to your Firebase project.
 
 /******************************************************
  * FIREBASE CONFIGURATION - EDIT WITH YOUR DATA
  ******************************************************/
 
+ /* apiKey - The public API key of the project.
+  * Can be found in the project's settings in the Firebase Console.
+
+  * databaseURL - The URL of the project's database.
+  * Can be found by navigating to the real-time database in the Firebase Console.
+
+  * storageBucket - The URL of the project's storage bucket.
+  * Can be found by navigating to Storage in the Firebase Console.
+ */
+
 var config = {
-    apiKey: "",       // TODO: Your key goes here,
-    databaseURL: "",  // TODO: "https://YourProject.firebaseio.com/"
-    storageBucket: "" // TODO: "gs://language-processing-lab.appspot.com"
+    apiKey:        "",       // TODO: Your key goes here
+    databaseURL:   "",  // TODO: "https://your-project.firebaseio.com/"
+    storageBucket: "" // TODO: "gs://your-project.appspot.com"
 };
 
 
